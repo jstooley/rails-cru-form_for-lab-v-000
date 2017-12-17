313124bbdcc4cfe951599723eedd600dd7a631b5
Justin Tooley <jstooley823@gmail.com>
2017-12-17 18:54:10 +0000

    automatically backed up by learn

--- app/controllers/artists_controller.rb
+++ app/controllers/artists_controller.rb
@@ -24,6 +24,7 @@ class ArtistsController < ApplicationController
 
   def update
     @artist = Artist.find(params[:id]
+    @artist.update(artists_params(:name, :bio))
     @artist.save
     redirect_to artist_path(@artist)
   end
